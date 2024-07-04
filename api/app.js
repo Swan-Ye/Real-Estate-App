c0fbf6310e81cd96d3bd4a18730080544e73c49e
swanyeehtet <swanyeehtet1@mytel.com.mm>
2024-07-04 11:27:10 +0630

    api finished

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import postRoute from "./routes/post.route.js"
 import authRoute from "./routes/auth.route.js"
 
 const app = express();
+// require('dotenv').config();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/posts", postRoute)
 app.use("/api/auth", authRoute)
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -30,10 +31,46 @@ export const register = async (req, res) => {
 
 }
 
-export const login = (req, res) => {
-    //db operations
+export const login = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+
+        //CHECK IF THE USER EXISTS
+
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+
+        if (!user) return res.status(401).json({ message: "Invalid Crendentials!" })
+
+        //CHECK IF THE PASSWORD IS CORRECT
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!password) return res.status(401).json({ message: "Invalid Crendentials!" })
+
+        //GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+        // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+        const age = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign({
+            id: user.id
+        }, process.env.JWT_SECRET_KEY, { expiresIn: age })
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true,
+            maxAge: age,
+        }).status(200).json({ message: "Login Successful" })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Failed to login!" })
+    }
 }
 
 export const logout = (req, res) => {
-    //db operations
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" })
 }
