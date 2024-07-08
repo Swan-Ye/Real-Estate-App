2293cc0ac87e48818c638e6bda086a9a6568b853
swanyeehtet <swanyeehtet1@mytel.com.mm>
2024-07-08 10:31:32 +0630

    homepage responsive done

---
+++ client/real-estate/src/routes/homePage/homePage.jsx
@@ -0,0 +1,35 @@
+import SearchBar from "../../components/searchBar/searchBar"
+import "./homePage.scss"
+
+function homePage() {
+    return (
+        <div className="homePage">
+            <div className="textContainer">
+                <div className="wrapper">
+                    <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae odio quis exercitationem ipsam quod possimus alias quibusdam iste libero illum quasi, praesentium iusto modi fuga cumque necessitatibus ut? Odio.</p>
+                    <SearchBar />
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>16+</h1>
+                            <h2>Years of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Award Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>2000+</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default homePage
